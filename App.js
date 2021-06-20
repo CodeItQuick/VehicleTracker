5d57cd69bb265d4a5f046a4d4df436e8f758a89d
Steven R. Baker <steven@stevenrbaker.com>
2021-06-20 12:45:12 +0200

    Add an entry creation form.

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   FlatList,
   Button,
   Text,
+  TextInput,
   View,
 } from 'react-native';
 
@@ -49,7 +50,40 @@ const EntryListScreen = ({ navigation }) => {
 };
 
 const AddEntryScreen = ({ navigation }) => {
-  return <Text>This is the add entry screen</Text>;
+  return (
+    <View testID="addEntryForm">
+      <TextInput
+        testID="dateInput"
+        label="Date"
+        autoCapitalize="none"
+      />
+      <TextInput
+        testID="odometerInput"
+        label="Odometer"
+        autoCapitalize="none"
+      />
+      <TextInput
+        testID="fuelAmountInput"
+        label="Fuel Amount(L)"
+        autoCapitalize="none"
+      />
+      <TextInput
+        testID="fuelPriceInput"
+        label="Fuel Price (per L)"
+        autoCapitalize="none"
+      />
+      <TextInput
+        testID="totalPriceInput"
+        label="Total Price"
+        autoCapitalize="none"
+      />
+      <Button
+        onPress={() => navigation.goBack()}
+        title="Save"
+        accessibilityLabel="Save this entry to the log."
+      />
+    </View>
+  );
 };
 
 const App = () => {
--- e2e/addEntryTest.e2e.js
+++ e2e/addEntryTest.e2e.js
@@ -10,4 +10,20 @@ describe('Add an Entry', () => {
   it('has an add entry button', async () => {
     await expect(element(by.id('addEntryButton'))).toBeVisible();
   });
+
+  it('returns to the entrylist screen after saving an entry', async () => {
+    await element(by.id('addEntryButton')).tap();
+
+    await expect(element(by.id('addEntryForm'))).toBeVisible();
+
+    await element(by.id('dateInput')).typeText('2020-06-18');
+    await element(by.id('odometerInput')).typeText('123455');
+    await element(by.id('fuelAmountInput')).typeText('75,24');
+    await element(by.id('fuelPriceInput')).typeText('15,75');
+    await element(by.id('totalPriceInput')).typeText('1185,19');
+
+    await element(by.text('Save')).tap();
+
+    await expect(element(by.id('entriesList'))).toBeVisible();
+  });
 });
