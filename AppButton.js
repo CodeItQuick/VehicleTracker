aef6b66fddf9d9349d9cbeb18782509c21a4c192
Steven R. Baker <steven@stevenrbaker.com>
2021-06-22 01:21:22 +0200

    Add license header to the source files.

--- AppButton.js
+++ AppButton.js
@@ -1,3 +1,22 @@
+//
+// Copyright (c) 2021 Steven R. Baker <steven@stevenrbaker.com>
+//
+// This file is part of VehicleTracker.
+//
+// VehicleTracker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VehicleTracker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VehicleTracker.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 import React from 'react';
 import {
   Text,
